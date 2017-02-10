5643e5b863af6bb3cb32ea99b324440ddd4c27a1
R-Divya <divyaram4444@gmail.com>
2017-02-10 16:03:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,12 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//move the image
+var img=document.getElementById('img');
+img.onclick = function (){
+  
+  img.style.marginLeft = '100px';  
+    
+    
+};
