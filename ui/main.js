b0cdae88cc18b1e73071ef0260696297c3b037fa
R-Divya <divyaram4444@gmail.com>
2017-02-12 21:08:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,24 @@
 //counte code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('counter');
+    var request = new  XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+       if(request.state === 200 ){
+       var   counter = request.responseText;
+        var span = document.getElementById('counter');
     span.innerHTML = counter.toString();
+           
+           
+       }
+      }
+        
+        
+        
+    };
+    
+   request.open('GET','htpp://r-divya.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
