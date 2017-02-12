4391c68e5284d5ceb6b7cb651b23f44380227991
R-Divya <divyaram4444@gmail.com>
2017-02-12 22:53:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,7 @@ button.onclick = function () {
       if(request.readyState === XMLHttpRequest.DONE){
           
        if(request.status === 200 ) {
-     var counter = request.responseText;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+     
            
            }
       }
@@ -23,7 +21,7 @@ button.onclick = function () {
         
     };
     
-   request.open(`GET`, `http://r-divya.imad.hasura-app.io/counter`,true);
+   request.open(`GET`, `http://r-divya.imad.hasura-app.io/submit-name?name=` + name,true);
    request.send(null);
 };
 
@@ -33,7 +31,26 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
-   var names = ['name1','name2','name3','name4'];
+    var request = new  XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          
+       if(request.status === 200 ) {
+     
+           
+           }
+      }
+        
+        
+        
+    };
+     request.open(`GET`, `http://r-divya.imad.hasura-app.io/submit-name?name=` + name,true);
+   request.send(null);
+};
+    
+    
+   var names = request.responseText;
+   names = JSON.parse(names);
    var list = '';
    for(var i=0;i< names.length;i++)
 {
