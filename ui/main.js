6c553fc230cb7c2295b36558804245595763327a
R-Divya <divyaram4444@gmail.com>
2017-02-12 22:58:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,6 +37,19 @@ submit.onclick = function () {
           
        if(request.status === 200 ) {
      
+     var names = request.responseText;
+   names = JSON.parse(names);
+   var list = '';
+   for(var i=0;i< names.length;i++)
+{
+    list +='<li>'+names[i] + '</li>';
+}
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+     
+     
+     
+     
            
            }
       }
@@ -49,13 +62,4 @@ submit.onclick = function () {
 };
     
     
-   var names = request.responseText;
-   names = JSON.parse(names);
-   var list = '';
-   for(var i=0;i< names.length;i++)
-{
-    list +='<li>'+names[i] + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
-};
+   
