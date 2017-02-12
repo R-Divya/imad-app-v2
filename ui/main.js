7afa42ce3ab56cc2cc50aa9063264800dccd2141
R-Divya <divyaram4444@gmail.com>
2017-02-12 21:50:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ button.onclick = function () {
         
     };
     
-   request.open('GET', 'http://r-divya.imad.hasura-app.io/counter',true);
+   request.open(`GET`, `http://r-divya.imad.hasura-app.io/counter`,true);
    request.send(null);
 };
 
