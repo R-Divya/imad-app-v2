d3901e91114d396ee7853cf843c30b40bad806f5
R-Divya <divyaram4444@gmail.com>
2017-02-16 21:09:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function () {
 };
 
   
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
@@ -53,6 +51,8 @@ submit.onclick = function () {
      };
 
            
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open(`GET`, `http://r-divya.imad.hasura-app.io/submit-name?name=` + name ,true);
    request.send(null);
    
