69b4214a3988101ad2617ecfb5ffe863d1de96df
R-Divya <divyaram4444@gmail.com>
2017-02-16 21:01:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,32 +58,5 @@ submit.onclick = function () {
    
       };
      
-  var names=['name1','name2','name3','name4'];
-  var list = '';
-   for(var i=0;i< names.length;i++){
-       list +='<li>'+ names[i] + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
-};    
-     
-     
-     
-           
-          
-        
-        
-        
-   
-    var nameInput = document.getElementById('name');
-var name = nameInput.value;
-    
-    
-    
-    
-     request.open(`GET`, `http://r-divya.imad.hasura-app.io/submit-name?name=` + name,true);
-   request.send(null);
-};
-    
-    
+  
    
